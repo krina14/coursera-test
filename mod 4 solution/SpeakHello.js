380e64db04ab9c75f2715dde097b2b947e8373d3
krina14 <67433095+krina14@users.noreply.github.com>
2020-08-13 19:24:10 +0530

    Update SpeakHello.js

--- mod 4 solution/SpeakHello.js
+++ mod 4 solution/SpeakHello.js
@@ -1,12 +1,8 @@
 (function(window){
 	var helloSpeaker={};
-	var speakWord = "Hello";
-	helloSpeaker.sayhello=function(){
-		function speak(name) {
+	var speakWord= "Hello";
+	helloSpeaker.speak=function(name){
   		console.log(speakWord + " " + name);
 		}	
 		window.helloSpeaker=helloSpeaker;
-	}
-
-
 })(window);
