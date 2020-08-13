3b89326df10cd785d954a99f68bcfd2f7cca9a6a
krina14 <67433095+krina14@users.noreply.github.com>
2020-08-13 19:00:10 +0530

    Update SpeakGoodBye.js

--- mod 4 solution/SpeakGoodBye.js
+++ mod 4 solution/SpeakGoodBye.js
@@ -1,14 +1,12 @@
 (function(window){
-	var speakWord = "Good Bye"	;
-
-	function speak(name) {
-  	console.log(speakWord + " " + name);
+	var byeSpeaker={};
+	var speakWord = "Good Bye";
+	byeSpeaker.SpeakGoodBye=function(){
+		function speak(name) {
+  		console.log(speakWord + " " + name);
 		}
+		window.byeSpeaker=byeSpeaker;
+	}
+	
 	
 })(window);
-
-
-
-// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-// 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
