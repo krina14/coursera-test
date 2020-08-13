a5e7457bc195ab9cfe31113878fd2d06cb45f439
krina14 <67433095+krina14@users.noreply.github.com>
2020-08-13 18:55:24 +0530

    Add files via upload

---
+++ mod 4 solution/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+(function(window){
+	var speakWord = "Good Bye"	;
+
+	function speak(name) {
+  	console.log(speakWord + " " + name);
+		}
+	
+})(window);
+
+
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
