e666904d7098ea48e0d671b0d653fa20903caa06
krina14 <67433095+krina14@users.noreply.github.com>
2020-08-13 19:23:36 +0530

    Update SpeakGoodBye.js

--- mod 4 solution/SpeakGoodBye.js
+++ mod 4 solution/SpeakGoodBye.js
@@ -1,12 +1,8 @@
 (function(window){
 	var byeSpeaker={};
 	var speakWord = "Good Bye";
-	byeSpeaker.SpeakGoodBye=function(){
-		function speak(name) {
+	byeSpeaker.speak=function(name){
   		console.log(speakWord + " " + name);
 		}
 		window.byeSpeaker=byeSpeaker;
-	}
-	
-	
 })(window);
